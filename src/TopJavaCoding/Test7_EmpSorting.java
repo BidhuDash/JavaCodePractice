e8a160c6e8ac49991233f02ba0a5e81a9d8d7761
Bidhu@Desktop <bbd.papun@gmail.com>
2024-06-05 22:56:08 +0530

    Merge remote-tracking branch 'origin/main'

--- src/TopJavaCoding/Test7_EmpSorting.java
+++ src/TopJavaCoding/Test7_EmpSorting.java
@@ -1,7 +1,6 @@
 package TopJavaCoding;
 
 import java.util.ArrayList;
-
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -67,6 +66,7 @@ class Emp {
 
 
 public class Test7_EmpSorting {
+
     public static void main(String[] args) {
 
         List<Emp> emp = new ArrayList<>();
@@ -74,12 +74,12 @@ public class Test7_EmpSorting {
         emp.add(new Emp("Song",27,3L,65623.00));
         emp.add(new Emp("Lister",28,4L,542332.65));
         emp.add(new Emp("Dashing",29,5L,23463.54));
-        emp.add(new Emp("Toy",66,8L,7845.58));
-        emp.add(new Emp("Wolf",45,1L,9896.99));
-        emp.add(new Emp("Boat",789,6L,77144.71));
+        emp.add(new Emp("Toy",66,8L,78425.58));
+        emp.add(new Emp("Wolf",45,1L,989226.99));
+        emp.add(new Emp("Boat",789,6L,771144.71));
         emp.add(new Emp("Tin",85,11L,325698.98));
-        emp.add(new Emp("Rubber",76,10L,8923463.45));
-        emp.add(new Emp("Light",999,55L,9923463.78));
+        emp.add(new Emp("Rubber",76,10L,892463.45));
+        emp.add(new Emp("Light",999,55L,992343.78));
 
 
 //        List<Emp> list = emp.stream().filter(x->x.getAge()>27).toList();
@@ -91,11 +91,16 @@ public class Test7_EmpSorting {
 //                age.set(x.getAge());
 //            }
 //        });
+//        System.out.println(age);
 
         System.out.println(emp.stream().collect(Collectors.groupingBy(Emp::getName,Collectors.counting())));
-       List<String> filter= emp.stream().filter(x->x.getSalary()>40000).sorted(Comparator.comparingDouble(Emp::getSalary)).limit(3).map(Emp::getName).toList();
-        System.out.println(filter);
-//        System.out.println(age);
+
+        // list to get Emp names who are having sal>40000, limit = first 3 employees,
+       List<String> filter= emp.stream().filter(x->x.getSalary()>40000)
+               .sorted(Comparator.comparingDouble(Emp::getSalary)).limit(3)
+               .map(Emp::getName).toList();
+
+       System.out.println(filter);
     }
 
 //    select emp.name, dept.name, emp.sal from dept Inner join on dept  emp.name=dept.name;
--- src/TopJavaCoding/Test9_Frequency.java
+++ src/TopJavaCoding/Test9_Frequency.java
@@ -1,15 +1,12 @@
 package TopJavaCoding;
 
-import jdk.jfr.Frequency;
-
 import java.util.*;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
-public class Test9 {
+public class Test9_Frequency {
     public static void main(String[] args) {
 
-        List<Integer> list = Arrays.asList(100,5,-2,-7,-3, 5,4,6,7);
+        List<Integer> list = Arrays.asList(100,100,5,-2,-7,7,-3,5,4,6,7);
 
         //Map<Integer,Integer> map =list.stream().collect(Collectors.toMap(Collections.frequency(Collections.,x->1,Integer::sum)));
 
