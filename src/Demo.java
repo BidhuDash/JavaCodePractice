a8a52898d430cff420946de01a5845c80caa86ca
Bidhu@Desktop <bbd.papun@gmail.com>
2025-06-05 01:46:52 +0530

    Merge remote-tracking branch 'origin/main'

--- src/Demo.java
+++ src/Demo.java
@@ -1,5 +1,4 @@
-import java.util.Map;
-import java.util.function.Function;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -8,16 +7,75 @@ import java.util.stream.Collectors;
  */
 public class Demo {
     public static void main(String[] args) {
-        String str = "safaewrwag";
+        Map<String, Integer> map = new HashMap<>();
+        map.put("Bidhu", 121);
+        map.put("Bhusan", 755);
+        map.put("Dash", 528);
+        map.put("Bbd", 332);
+        map.put("Papun", 198);
+        map.put("Kaliya", 565);
+        map.put("Bhusan", 744);
 
-        // find occurances of each character
+        List<Map.Entry<String, Integer>> list = new ArrayList<>(map.entrySet());
+//        list.sort((x, y) -> {
+//            int sal = x.getValue().compareTo(y.getValue());
+//            if (sal == 0) {
+//                return x.getKey().compareTo(y.getKey());
+//            }
+//            return sal;
+//        });
 
-        Map<Character, Long> occ = str.chars().mapToObj(c -> (char) c)
-                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        list.sort(Map.Entry.comparingByValue((x, y) -> y - x));
 
-        //char frq = occ.values().stream().max(Integer::compare).toString();
+        Map<String, Integer> sortedList = new LinkedHashMap<>();
+        for (Map.Entry<String, Integer> i : list) {
+            sortedList.put(i.getKey(), i.getValue());
+        }
+        //sortedList.forEach((key, value) -> System.out.println(key + "," + value));
+
+        List<Karyakarta> emp = new ArrayList<>();
+        emp.add(new Karyakarta("Bidhu", 121.66));
+        emp.add(new Karyakarta("Bhusan", 755.30));
+        emp.add(new Karyakarta("Dash", 528.76));
+        emp.add(new Karyakarta("Bbd", 332.33));
+        emp.add(new Karyakarta("Papun", 198.65));
+        emp.add(new Karyakarta("Kaliya", 565.31));
+        emp.add(new Karyakarta("Bhusan", 744.23));
 
 
-        occ.forEach((x,y)-> System.out.println(x+","+y));
+        Map<Double, String> empMap =
+                emp.stream().filter(x-> x.getSalary() > 200).collect(Collectors.toMap(Karyakarta::getSalary, Karyakarta::getName,
+                        (s1, s2) -> s2));
+        List<Map.Entry<Double, String>> sortedList2 =
+                empMap.entrySet().stream().sorted(Map.Entry.comparingByKey(Double::compareTo)).toList();
+        sortedList2.forEach((x) -> System.out.println(x.getValue() + " , sal " + x.getKey()));
+
+    }
+}
+
+
+class Karyakarta {
+    private String name;
+    private Double salary;
+
+    public Karyakarta(String name, Double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
     }
 }
--- src/DesignPatterns/Creational/Singleton.java
+++ src/DesignPatterns/Creational/Singleton.java
@@ -2,6 +2,7 @@ package DesignPatterns.Creational;
 
 class Singleton {
     private static Singleton singletonInstance;
+   // private static final Singleton singletonInstance = new Singleton();  // for Eager Instantiation
     public String s;
 
     private Singleton(){
@@ -10,34 +11,35 @@ class Singleton {
     }
 
     // TYPE 1---- use this method technique for early instantiation or eager initialization
-    /*public static Singleton getInstance(){
-        return singleton;
+   /* public static Singleton getInstance(){
+        return singletonInstance;
     }*/
 
     // TYPE 1(style 2)---- use this for without using getInstance()
-    /*static{
+    static{
         singletonInstance = new Singleton();
-    }*/
+    }
 
-    // TYPE 2---- use this technique for lazy instantiation, or when the object is needed then it will be created
-    public static Singleton getInstance(){
+    // TYPE (2)---- use this technique for lazy instantiation, or when the object is needed then it will be created
+    /*public static Singleton getInstance(){
         if(singletonInstance == null){
             singletonInstance = new Singleton();
         }
         return singletonInstance;
-    }
+    }*/
 
 
-    //Type 3---- use this technique for thread safe singleton usage, and the performance is also slow
-    /*public static synchronized Singleton threadSafeSingleton(){
+    //Type (3)---- use this technique for thread safe singleton usage, and the performance is also slow
+    /*public static synchronized Singleton threadSafeSingletonInstance(){
         if(singletonInstance==null)
             singletonInstance = new Singleton();
 
         return singletonInstance;
     }*/
 
-    // Type 4--- using this technique double-checked locking principle is achieved
-    /*public static Singleton SingletonInstance() {
+
+    // Type (4)--- using this technique double-checked locking principle is achieved
+   /* public static Singleton SingletonInstance() {
         if(singletonInstance==null){
             synchronized(Singleton.class) {
                 singletonInstance = new Singleton();
@@ -52,6 +54,6 @@ class Singleton {
 
     public static void main(String[] args) {
         //Singleton sg = new Singleton();
-        getInstance();
+        //getInstance();
     }
 }
